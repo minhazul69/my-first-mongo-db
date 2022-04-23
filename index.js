38b267a19b65ef4b7c3e0003eb35f3557afac5ab
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-23 11:42:11 +0600

    userCollection add on mongodb

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const uri =
   "mongodb+srv://dbusername01:PwoyUhdkoLH4EGnR@cluster0.dbx4u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
@@ -10,13 +10,18 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  console.log("db conected");
-  // perform actions on the collection object
-  client.close();
-});
-
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("foodExpress").collection("user");
+    const user = { name: "Minhajul Alam", email: "minhajul@gmail.com" };
+    const result = await userCollection.insertOne(user);
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+    // await client.close()
+  }
+}
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Mongo Db first website");
 });
