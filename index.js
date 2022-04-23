4cb67dc1cb08b23e627fab5a259195e8cb272ea4
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-23 15:39:57 +0600

    user add and delete on database

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-var cors = require("cors");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 app.use(cors());
 app.use(express.json());
 const uri =
@@ -17,12 +18,26 @@ async function run() {
   try {
     await client.connect();
     const userCollection = client.db("foodExpress").collection("user");
+    // GET USER
+    app.get("/user", async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const user = await cursor.toArray();
+      res.send(user);
+    });
+    // POST USER
     app.post("/user", async (req, res) => {
       const user = req.body;
       console.log(user);
       const result = await userCollection.insertOne(user);
       res.send(result);
-      // console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    });
+    // DELETE USER
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
     });
   } finally {
     // await client.close()
