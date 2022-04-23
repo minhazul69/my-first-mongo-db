5952d1b699d6045838a02e47862a214343e7fd9d
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-23 13:08:08 +0600

    user added on mongodb

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+var cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-
+app.use(cors());
+app.use(express.json());
 const uri =
   "mongodb+srv://dbusername01:PwoyUhdkoLH4EGnR@cluster0.dbx4u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
@@ -10,18 +12,24 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
 async function run() {
   try {
     await client.connect();
     const userCollection = client.db("foodExpress").collection("user");
-    const user = { name: "Minhajul Alam", email: "minhajul@gmail.com" };
-    const result = await userCollection.insertOne(user);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+      // console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    });
   } finally {
     // await client.close()
   }
 }
 run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Mongo Db first website");
 });
